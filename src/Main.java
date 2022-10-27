bfa826ba3cdc0298b04ee7a7837ac465649fbb87
Andrei-Zmn <andrei.zeman@yahoo.com>
2022-10-27 19:16:14 +0300

    New

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! And a happy new day!");
+        System.out.println("Hello world! And a happy new day! fdgdfzgd gzsg");
     }
 }
